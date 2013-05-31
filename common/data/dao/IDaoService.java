9a74078aef41f61336c459f3513641ce24cb5435
jackeyzhang <zhangchenhao@139.com>
2013-05-31 17:28:47 +0800

    add some service itf and mysqljdbc.jar

---
+++ common/data/dao/IDaoService.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package data.dao;
+
+
+/**
+ * dao service
+ * 
+ * @author zhangchenhao
+ *
+ *
+ * 2013-5-31
+ * 下午5:03:08
+ *
+ * TODO:
+ */
+public interface IDaoService<T>{
+	
+	
+
+}
