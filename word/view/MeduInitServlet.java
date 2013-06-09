8baa284c697e83bf7e082a807fe92a67d2f057b6
wei binbin <wbbcg4426493@163.com>
2013-06-09 15:48:06 +0800

    servlet
    servlet

---
+++ word/view/MeduInitServlet.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package view;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * @author weibinbin
+ *
+ *
+ * Jun 9, 2013
+ * 3:44:50 PM
+ *
+ * TODO:
+ */
+public class MeduInitServlet extends HttpServlet {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    
+    
+
+}
