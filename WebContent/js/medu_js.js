f5fb3061be21f67630784d315d07be79849c1ef0
jackeyzhang <zhangchenhao@139.com>
2013-06-13 17:07:17 +0800

    调整代码

--- WebContent/js/medu_js.js
+++ WebContent/js/medu_js.js
@@ -1,1 +1,19 @@
 //js 代码放在这里
+
+ $(function() {
+		  $( "#tabs" ).tabs({
+			  event: "mouseover"
+		  });
+			
+		  $("#searchbutton").click(function(){
+			 $.post("meduInit",
+				  {
+				    name:"Donald Duck",
+				    city:"Duckburg"
+				  },
+				  function(data,status){
+				    alert("Data: " + data + "\nStatus: " + status);
+				  });
+		  });
+
+});
