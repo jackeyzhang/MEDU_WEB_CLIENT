8282fb47a3cce7d09a2a4d01c4094522684a13e4
jackeyzhang <zhangchenhao@139.com>
2013-06-21 17:07:00 +0800

    增加后台的一些文件

---
+++ backgroud/com/sickle/medu/bg/client/BgClient.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.sickle.medu.bg.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+
+/**
+ * medu 后台入口类
+ * 
+ * @author zhangchenhao
+ *
+ *
+ * 2013-6-21
+ * 下午4:42:51
+ *
+ * TODO:
+ */
+public class BgClient implements EntryPoint
+{
+
+	@Override
+	public void onModuleLoad( )
+	{
+
+	}
+
+}
