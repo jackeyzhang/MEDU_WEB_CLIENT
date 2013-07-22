dcd7e8ea621380d8657baad3972be0b6567e6aec
wei binbin <wbbcg4426493@163.com>
2013-07-22 14:34:40 +0800

    cache
    这个cache写的实在不怎么样

--- common/ui/context/ActionContext.java
+++ common/ui/context/ActionContext.java
@@ -3,15 +3,42 @@
  */
 package ui.context;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
 /**
  * @author weibinbin
- *
- *
- * Jul 17, 2013
- * 5:26:10 PM
- *
- * TODO:
+ * 
+ * 
+ *         Jul 17, 2013 5:26:10 PM
+ * 
+ *         TODO:
  */
-public class ActionContext {
+public class ActionContext implements ServletContextListener {
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * javax.servlet.ServletContextListener#contextInitialized(javax.servlet
+	 * .ServletContextEvent)
+	 */
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		ServletContext context = arg0.getServletContext();
+		PageCacheManager manger = PageCacheManager.getInstance().getInstance();
+		manger.initPageCache();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.servlet.ServletContextListener#contextDestroyed(javax.servlet.
+	 * ServletContextEvent)
+	 */
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+
+	}
 
 }
--- common/ui/context/PageCacheManager.java
+++ common/ui/context/PageCacheManager.java
@@ -3,29 +3,54 @@
  */
 package ui.context;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import ui.Constant;
+
+import com.sickle.dao.SchoolDaoService;
+import com.sickle.pojo.edu.School;
+import com.sickle.service.itf.ISchoolService;
+
 /**
  * @author weibinbin
- *
- *
- * Jul 18, 2013
- * 1:01:12 PM
- *
- * TODO:
+ * 
+ * 
+ *         Jul 18, 2013 1:01:12 PM
+ * 
+ *         TODO:
  */
 public class PageCacheManager {
 
-	PageCache cache = PageCache.getInstance();
+	public static PageCacheManager manger = new PageCacheManager();
 	
+	private Map<String, List<?>> cacheMap = new HashMap<String, List<?>>();
+
+	private ISchoolService schoolService = SchoolDaoService.getInstance();
+
+	public static PageCacheManager getInstance(){
+		return manger;
+	}
 	
 	/**
 	 * init cacheMap
 	 */
 	public void initPageCache() {
-		//
+		List<School> sch = schoolService.getAllSchool();
+		if (sch != null && sch.size() != 0) {
+			cacheMap.put(Constant.PAGECONTENT, sch);
+		}
 	}
-	
-	//update pagecontent
-	public void updatePageContent(){
+
+	// get page content
+	public synchronized List<School> getPageContent() {
+		return (List<School>) cacheMap.get(Constant.PAGECONTENT);
+	}
+
+	// update pagecontent
+	public synchronized void updatePageContent() {
+		//重新加载一遍
 		
 	}
 }
