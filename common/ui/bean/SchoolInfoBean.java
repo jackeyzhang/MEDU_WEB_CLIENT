93939401966b7d2c1d8a14c4c180a0be8cce0d1f
wei binbin <wbbcg4426493@163.com>
2013-07-29 10:20:10 +0800

    page change
    page change

---
+++ common/ui/bean/SchoolInfoBean.java
@@ -0,0 +1,96 @@
+/**
+ * 
+ */
+package ui.bean;
+
+/**
+ * @author weibinbin
+ *
+ *
+ * Jul 26, 2013
+ * 4:59:12 PM
+ *
+ * TODO:
+ */
+public class SchoolInfoBean {
+
+	private Integer id;
+	
+	private String name;
+	
+	private String city;
+	
+	private String imageurl;
+	
+	private String description;
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the city
+	 */
+	public String getCity() {
+		return city;
+	}
+
+	/**
+	 * @param city the city to set
+	 */
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	/**
+	 * @return the imageurl
+	 */
+	public String getImageurl() {
+		return imageurl;
+	}
+
+	/**
+	 * @param imageurl the imageurl to set
+	 */
+	public void setImageurl(String imageurl) {
+		this.imageurl = imageurl;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
